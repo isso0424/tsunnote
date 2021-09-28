b027933336c3c6dfacf995dcbb71d5f521b87d7f
isso0424 <isso04240129@gmail.com>
2021-09-29 03:23:16 +0900

    build: setup eslint

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
+/* eslint-disable */
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
 };
+/* eslint-enable */
