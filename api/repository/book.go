19ba98b1ab9de2847a3597c8155719c95ede0bdd
isso0424 <isso04240129@gmail.com>
2021-10-18 23:59:39 +0900

    feat: create books repository

--- api/repository/book.go
+++ api/repository/book.go
@@ -5,6 +5,17 @@ import (
 	"isso0424/tsunnote_api/repository/err"
 )
 
+type bookSortType int
+
+const (
+	CreatedAt = iota
+	PageCount
+)
+
 type BookRepository interface {
-	Create(isbnCode, title string, pageCount int) (domain.Book, err.Error)
+	CreateWithISBN(isbnCode string, pageCount int) (domain.Book, err.Error)
+	Create(title string, pageCount int) (domain.Book, err.Error)
+	Delete(id string) (domain.Book, err.Error)
+	FetchByID(id string, sort bookSortType) (domain.Book, err.Error)
+	FetchISBNCode(code string, sort bookSortType) (domain.Book, err.Error)
 }
